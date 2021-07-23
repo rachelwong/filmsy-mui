251ea28218014d1147fa1a5f321ea369ca9fa04b
rachel-wong <hello.rachelwong@gmail.com>
2021-07-23 13:09:47 +1000

    Fixed my stupidity with context api

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -3,8 +3,10 @@ import AppReducers from './AppReducers'
 
 // TODO - add data persistence with localstorage temporarily
 const initialState = {
-  bookmarks: localStorage.getItem("bookmarks") ? JSON.parse(localStorage.getItem("bookmarks")) : [],
-  completed: localStorage.getItem("completed") ? JSON.parse(localStorage.getItem("completed")) : []
+  // bookmarks: [],
+  // completed: []
+  bookmarks: localStorage.getItem('bookmarks') ? JSON.parse(localStorage.getItem('bookmarks')) : [],
+  completed: localStorage.getItem('completed') ? JSON.parse(localStorage.getItem('completed')) : []
 }
 
 // global state which will be accessible from anywhere
@@ -17,19 +19,19 @@ export const GlobalProvider = ({ children }) => {
 
   // everytime the state updates (add/remove), the localstorage is updated
   useEffect(() => {
-    localStorage.setItem("bookmarks", JSON.stringify(state.bookmarks))
-    localStorage.setItem("completed", JSON.stringify(state.completed))
+    localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks))
+    localStorage.setItem('completed', JSON.stringify(state.completed))
   }, [state])
 
   // actions to be - add bookmark, remove bookmark
   const removeBookmark = id => {
     console.log("REMOVE BOOKMARK", id)
-    dispatch({ type: "REMOVE_BOOKMARK", action: id })
+    dispatch({ type: "REMOVE_BOOKMARK", payload: id })
   }
 
   const addBookmark = movie => {
     console.log("ADD BOOKMARK", movie)
-    dispatch({ type: "ADD_BOOKMARK", action: movie })
+    dispatch({ type: "ADD_BOOKMARK", payload: movie })
   }
 
   // value argumment is what's available to be passed down to/accessed by the whole app
